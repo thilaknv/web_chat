d4314b0a8973fa6cd49ef505520b5cdbf3bd65e5
Thilak <thilakgowdanv@gmail.com>
2023-12-24 13:37:49 +0530

    host bug

--- index.js
+++ index.js
@@ -26,12 +26,7 @@ const UserState = {
     }
 }
 
-const io = new Server(expressServer, {
-    cors: {
-        origin: process.env.NODE_ENV === "production" ? false :
-            ["http://localhost:3500", "http://127.0.0.1:3500", "http://localhost:5500", "http://127.0.0.1:5500", "https://web-chat-app-socket-io.vercel.app/"]
-    }
-});
+const io = new Server(expressServer, { port });
 
 io.on('connection', socket => {
     // upon connection - only to user
--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-const socket = io('ws://localhost:3500');
+const socket = io();
 
 const msgInput = document.querySelector("#msgInput");
 const username = document.querySelector("#username");
